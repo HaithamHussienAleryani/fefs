23617530c384c1c73a2706c2cf0a2b044b45d93f
heryani <heryani@fa.gov.sa>
2026-08-03 17:09:45 +0300

    Add SQLite support for visitor tracking and implement graceful shutdown for WebSocket server. Update package.json and package-lock.json to include sqlite3 as a dependency.

--- app.js
+++ app.js
@@ -6,11 +6,20 @@ app.get("/", (req, res) => {
   res.sendFile("index.html", { root: __dirname });
 });
 
+process.on("SIGINT", function interrupt(ss) {
+  wss.clients.forEach(function each(client) {
+    client.close();
+  });
+  server.close(() => {
+    shutdownDB();
+  });
+});
+
 server.on("request", app);
 server.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
-
+// start websockets
 const WebSocket = require("ws").Server;
 
 const wss = new WebSocket({ server });
@@ -25,6 +34,10 @@ wss.on("connection", function connection(ws) {
     ws.send("Welcome to haitham server");
   }
 
+  db.run(
+    `INSERT INTO visitors (count,time) VALUES (${connectonSize},datetime('now') )`,
+  );
+
   ws.on("close", function close() {
     wss.broadcast(`Current visitors ${connectonSize}`);
     console.log("Client has been disconnected");
@@ -34,3 +47,26 @@ wss.on("connection", function connection(ws) {
 wss.broadcast = function broadcast(data) {
   wss.clients.forEach((client) => client.send(data));
 };
+// end websockets
+
+const sqlite = require("sqlite3");
+const db = new sqlite.Database(":memory:");
+
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE visitors (
+    count INTEGER,
+    time TEXT
+    )
+    `);
+});
+
+function getCounts() {
+  db.each("SELECT * FROM visitors", (err, row) => console.log(row));
+}
+
+function shutdownDB() {
+  getCounts();
+  console.log("shutting db");
+  db.close();
+}
