afb38af87223cb967df180e6ec6c532711c91525
heryani <heryani@fa.gov.sa>
2026-08-03 15:51:35 +0300

    Add WebSocket support to the server and update index.html for client-side WebSocket connection. Include 'ws' as a dependency in package.json and package-lock.json.

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
 const express = require("express");
+const server = require("http").createServer();
 const app = express();
 
 app.get("/", (req, res) => {
   res.sendFile("index.html", { root: __dirname });
 });
 
-app.listen(3000, () => {
+server.on("request", app);
+server.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
+const WebSocket = require("ws").Server;
+
+const wss = new WebSocket({ server });
+
+wss.on("connection", function connection(ws) {
+  const connectonSize = wss.clients.size;
+  console.log(`Clients connected ${connectonSize}`);
+
+  wss.broadcast(`Current visitors ${connectonSize}`);
+
+  if (ws.readyState === ws.OPEN) {
+    ws.send("Welcome to haitham server");
+  }
+
+  ws.on("close", function close() {
+    wss.broadcast(`Current visitors ${connectonSize}`);
+    console.log("Client has been disconnected");
+  });
+});
+
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach((client) => client.send(data));
+};
